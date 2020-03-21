df512b62dabe2a70a42ab12ee79becf28d40cfcb
abelheredia <herediabravoabel@gmail.com>
2020-03-21 09:11:50 -0500

    actualización

---
+++ app/src/main/java/com/example/mapbox/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.mapbox;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+    }
+}
