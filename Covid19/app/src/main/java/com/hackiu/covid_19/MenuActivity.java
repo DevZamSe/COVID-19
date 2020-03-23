6a62f4535de4c41c5b55fc2057f018a25c7c9f09
NiloVila <NiloVila@e-quipu.pe>
2020-03-23 07:59:53 -0500

    23.03.2020

--- Covid19/app/src/main/java/com/hackiu/covid_19/MenuActivity.java
+++ Covid19/app/src/main/java/com/hackiu/covid_19/MenuActivity.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
-import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -16,7 +15,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
-import com.hackiu.covid_19.Watson.MainActivity;
 import com.mapbox.geojson.Feature;
 import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Point;
